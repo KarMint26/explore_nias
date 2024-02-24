7c57d3538157ea323ef62c638b63c6fa587777df
karel <kareltrisnanto26@gmail.com>
2024-02-24 16:23:07 +0700

    initial files

---
+++ app/wisata/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from 'next'
+import React from 'react'
+
+export const metadata: Metadata = {
+  title: "Wisata",
+  description: "Mengenal Lebih Dekat Nias dan Kekayaan Budayanya",
+}
+
+const Wisata = () => {
+  return (
+    <div>Wisata</div>
+  )
+}
+
+export default Wisata
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  // register: true,
+  // scope: "/app",
+  // sw: "service-worker.js",
+  //...
+});
+
+export default withPWA({
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+});

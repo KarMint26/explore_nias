73b02b92fe63055ebe5e09acd47064b19473137c
dodichakill <dodiherlangga91@gmail.com>
2024-03-02 11:54:43 +0700

    fix problem in routemaps

--- components/custom/RouteMaps.tsx
+++ components/custom/RouteMaps.tsx
@@ -26,11 +26,11 @@ function Item({ name, startPrice, endPrice }: ItemType) {
     <>
       {/* desktop */}
       <li className="mb-10 ms-6">
-        <span className="absolute flex items-center justify-center w-6 h-6 bg-mainLight rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-main"></span>
-        <h3 className="flex items-center mb-1 text-lg font-semibold text-main dark:text-white">
+        <span className="absolute flex items-center justify-center w-6 h-6 bg-main rounded-full -start-3 dark:ring-gray-900 dark:bg-main"></span>
+        <h3 className="flex items-center mb-1 text-lg font-semibold text-main dark:text-main">
           {name}
         </h3>
-        <p className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-300">
+        <p className="block mb-2 text-sm font-normal leading-none text-gray-500">
           IDR {startPrice} - {endPrice}
         </p>
       </li>
@@ -61,7 +61,7 @@ const dataMaps = [
       {
         id: 2,
         nameId: "Bandara Binaka Gunungsitoli",
-        nameEn: "BBinaka Gunungsitoli Airport",
+        nameEn: "Binaka Gunungsitoli Airport",
         startPrice: "1.000.000",
         endPrice: "1.500.000",
       },
@@ -131,7 +131,7 @@ const dataMaps = [
       {
         id: 2,
         nameId: "Bandara Binaka Gunungsitoli",
-        nameEn: "BBinaka Gunungsitoli Airport",
+        nameEn: "Binaka Gunungsitoli Airport",
         startPrice: "1.000.000",
         endPrice: "1.500.000",
       },
@@ -198,48 +198,50 @@ function RouteMaps() {
 
   return (
     <div className="w-full relative">
-      <div className="w-full bg-white  dark:bg-darkBg flex flex-col">
-        <Button
-          className="text-lg hover:bg-transparent bg-transparent shadow-none text-main self-center rounded-full px-10 py-5 mt-5"
-          onClick={() => setShowDetailRoute(true)}
-        >
-          <FiChevronsUp className="text-4xl font-bold text-black dark:text-white" />
-        </Button>
+      {!showDetailRoute && (
+        <div className="w-full bg-white  dark:bg-darkBg flex flex-col">
+          <Button
+            className="text-lg hover:bg-transparent bg-transparent shadow-none text-main self-center rounded-full px-10 py-5 mt-5"
+            onClick={() => setShowDetailRoute(true)}
+          >
+            <FiChevronsUp className="text-4xl font-bold text-black dark:text-white" />
+          </Button>
 
-        <h3 className="text-main text-2xl sm:text-3xl mb-8 text-center">
-          {locale === "id"
-            ? "Pilih Perjalananmu dari"
-            : "Choose Your Trip from"}
-        </h3>
+          <h3 className="text-main text-2xl sm:text-3xl mb-8 text-center">
+            {locale === "id"
+              ? "Pilih Perjalananmu dari"
+              : "Choose Your Trip from"}
+          </h3>
 
-        <div className="w-full py-20 px-16 mb-10 bg-mainLight dark:bg-submainDark items-center rounded-t-[5rem] flex flex-col sm:flex-row justify-between">
-          {dataMaps.map((item, index) => (
-            <div
-              className="w-80 relative cursor-pointer"
-              onClick={() => {
-                setDataRoute(item.timeLine);
-                setRouteActive(item.nameId);
-                setShowDetailRoute(true);
-              }}
-            >
-              <Image
-                src={item.image}
-                width={600}
-                height={600}
-                className="w-full scale-90 sm:scale-100 drop-shadow-lg"
-                alt={item.nameEn}
-              />
+          <div className="w-full py-20 px-16 mb-10 bg-[#5A574A] dark:bg-submainDark items-center rounded-t-[5rem] flex flex-col sm:flex-row justify-between">
+            {dataMaps.map((item, index) => (
+              <div
+                className="w-80 relative cursor-pointer"
+                onClick={() => {
+                  setDataRoute(item.timeLine);
+                  setRouteActive(item.nameId);
+                  setShowDetailRoute(true);
+                }}
+              >
+                <Image
+                  src={item.image}
+                  width={600}
+                  height={600}
+                  className="w-full scale-90 sm:scale-100 drop-shadow-lg"
+                  alt={item.nameEn}
+                />
 
-              <h3 className="text-white text-3xl absolute font-medium top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 drop-shadow-2xl">
-                {locale === "id" ? item.nameId : item.nameEn}
-              </h3>
-            </div>
-          ))}
+                <h3 className="text-white text-3xl absolute font-medium top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 drop-shadow-3xl">
+                  {locale === "id" ? item.nameId : item.nameEn}
+                </h3>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
+      )}
 
       {showDetailRoute && (
-        <div className="absolute bg-white dark:bg-darkBg top-0 right-0 flex flex-col items-center w-screen">
+        <div className="bg-white dark:bg-darkBg flex flex-col items-center w-screen">
           <Button
             className="text-lg hover:bg-transparent bg-transparent shadow-none text-main dark:text-white self-center rounded-full px-10 py-5 mt-5"
             onClick={() => setShowDetailRoute(false)}
@@ -247,45 +249,47 @@ function RouteMaps() {
             <FiChevronsDown className="text-4xl font-bold text-black dark:text-white" />
           </Button>
 
-          <h3 className="text-main text-xl sm:text-3xl mb-5 text-center">
+          <h3 className="text-main drop-shadow-md text-xl sm:text-3xl mb-5 text-center">
             {locale === "id"
               ? "Menjelajahi Pengalaman Berharga di Explorenias"
               : "Exploring the Precious Experience at Explorenias"}
           </h3>
 
-          <div className="flex flex-col-reverse justify-center sm:flex-row items-center">
-            <div className="roadmap sm:w-[40rem]">
-              <h3 className="text-xl font-medium sm:text-3xl text-main">
-                {routeActive}
-              </h3>
+          <div className="w-full bg-mainLight flex items-center justify-center pt-10 rounded-t-[5rem]">
+            <div className="flex w-1/2 flex-col-reverse justify-center sm:flex-row items-center">
+              <div className="roadmap sm:w-[40rem]">
+                <h3 className="text-xl font-medium sm:text-3xl text-main">
+                  {routeActive}
+                </h3>
 
-              <ol className="relative border-s border-black dark:border-gray-600 mt-5">
-                {dataRoute.map((item) => (
-                  <Item
-                    key={item.id}
-                    name={locale === "id" ? item.nameId : item.nameEn}
-                    startPrice={item.startPrice}
-                    endPrice={item.endPrice}
-                  />
-                ))}
-              </ol>
-            </div>
-            <div className="w-full">
-              <Image
-                src={
-                  routeActive === "Jakarta"
-                    ? "/assets/homepage/maps/jakarta.png"
-                    : routeActive === "Banten"
-                    ? "/assets/homepage/maps/banten.png"
-                    : routeActive === "Singapura"
-                    ? "/assets/homepage/maps/singapura.png"
-                    : "/assets/homepage/maps/dunia.png"
-                }
-                alt="maps"
-                width={500}
-                height={500}
-                className="w-full scale-75 object-cover"
-              />
+                <ol className="relative border-s border-black dark:border-gray-600 mt-5">
+                  {dataRoute.map((item) => (
+                    <Item
+                      key={item.id}
+                      name={locale === "id" ? item.nameId : item.nameEn}
+                      startPrice={item.startPrice}
+                      endPrice={item.endPrice}
+                    />
+                  ))}
+                </ol>
+              </div>
+              <div className="w-full">
+                <Image
+                  src={
+                    routeActive === "Jakarta"
+                      ? "/assets/homepage/maps/jakarta.png"
+                      : routeActive === "Banten"
+                      ? "/assets/homepage/maps/banten.png"
+                      : routeActive === "Singapura"
+                      ? "/assets/homepage/maps/singapura.png"
+                      : "/assets/homepage/maps/dunia.png"
+                  }
+                  alt="maps"
+                  width={500}
+                  height={500}
+                  className="w-full scale-75 object-cover"
+                />
+              </div>
             </div>
           </div>
         </div>
